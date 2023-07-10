d2b9c651be8b6bbfcdc1ea8f2cc8339578b34b15
John Doe <yamatogija@gmail.com>
2023-07-10 20:12:25 +0700

    Add Payment

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -181,7 +181,7 @@ class PaymentController extends Controller
     public function midtransCallback(Request $request)
     {
         $notif = $request->method() == 'POST' ? new Midtrans\Notification() : Midtrans\Transaction::status($request->order_id);
-        return $notif;
+        // return $notif;
         $transaction_status = $notif->transaction_status;
         $fraud = $notif->fraud_status;
 
