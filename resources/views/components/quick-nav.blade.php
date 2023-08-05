d3ec2217a97fb693e639d2a70c50e9cce589d2b2
John Doe <yamatogija@gmail.com>
2023-08-05 15:32:43 +0700

    Edit Navigation

--- resources/views/components/quick-nav.blade.php
+++ resources/views/components/quick-nav.blade.php
@@ -8,22 +8,30 @@
                 class="px-6 py-4 overflow-hidden text-lg font-bold transition-all duration-300 ease-in-out border-4 rounded-lg shadow-md hover:shadow-xl sm:px-14 nav-link">
                 <i class="mr-4 fa-brands fa-tiktok"></i> Sadina TikTok
             </a>
-            <a href="https://drive.google.com/drive/folders/12XWCEmJi2rqmHwY314G9UefewPCZaYvY" target="_blank"
+            {{-- <a href="https://drive.google.com/drive/folders/12XWCEmJi2rqmHwY314G9UefewPCZaYvY" target="_blank"
+                class="px-6 py-4 overflow-hidden text-lg font-bold transition-all duration-300 ease-in-out border-4 rounded-lg shadow-md hover:shadow-xl sm:px-14 nav-link">
+                <i class="mr-4 fa-solid fa-book"></i> Katalog Sadina
+            </a> --}}
+            <a href="{{ route('browse.index') }}" target="_blank"
                 class="px-6 py-4 overflow-hidden text-lg font-bold transition-all duration-300 ease-in-out border-4 rounded-lg shadow-md hover:shadow-xl sm:px-14 nav-link">
                 <i class="mr-4 fa-solid fa-book"></i> Katalog Sadina
             </a>
             <a href="https://www.facebook.com/silvia.noviani.7/videos" target="_blank"
                 class="px-6 py-4 overflow-hidden text-lg font-bold transition-all duration-300 ease-in-out border-4 rounded-lg shadow-md hover:shadow-xl sm:px-14 nav-link">
-                <i class="mr-4 fa-brands fa-facebook"></i> Update Produk di Facebook
+                <i class="mr-4 fa-brands fa-facebook"></i> Sadina Facebook
             </a>
+            {{-- <a href="https://www.facebook.com/silvia.noviani.7/videos" target="_blank"
+                class="px-6 py-4 overflow-hidden text-lg font-bold transition-all duration-300 ease-in-out border-4 rounded-lg shadow-md hover:shadow-xl sm:px-14 nav-link">
+                <i class="mr-4 fa-brands fa-facebook"></i> Update Produk di Facebook
+            </a> --}}
             <a href="https://goo.gl/maps/VdG99X3wE9C7RK4RA" target="_blank"
                 class="px-6 py-4 overflow-hidden text-lg font-bold transition-all duration-300 ease-in-out border-4 rounded-lg shadow-md hover:shadow-xl sm:px-14 nav-link">
                 <i class="mr-4 fa-solid fa-map-location-dot"></i> Lokasi Sadina
             </a>
-            <a href="https://forms.gle/QmmjfFP7Pq5fnEkK7" target="_blank"
+            {{-- <a href="https://forms.gle/QmmjfFP7Pq5fnEkK7" target="_blank"
                 class="px-6 py-4 overflow-hidden text-lg font-bold transition-all duration-300 ease-in-out border-4 rounded-lg shadow-md hover:shadow-xl sm:px-14 nav-link">
                 <i class="mr-4 fa-solid fa-circle-check"></i> Konfirmasi Alamat Pengiriman
-            </a>
+            </a> --}}
         </div>
     </div>
     <div class="flex justify-center p-2 mx-auto mt-6 overflow-hidden rounded-full w-fit toggle-navigation">
